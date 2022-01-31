b42471265a35e750d7e994693a2024e5228c134b
jenikeerthi723 <97227766+jenikeerthi723@users.noreply.github.com>
2022-01-31 17:57:20 +0530

    Add files via upload

---
+++ Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "bootstrap-icons/font/bootstrap-icons.css";
+function Footer(props) {
+  return (
+
+     <div className="footer">
+       <p><i class="bi bi-emoji-heart-eyes"></i>Google Keep App</p> 
+      </div>
+    
+  );
+}
+
+export default Footer;
---
+++ Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+function Header() {
+  return (
+    <div className="header">
+     <nav class="navbar navbar-light bg-light">
+     <div class="container-fluid">
+     <a class="navbar-brand" href="/">
+     <img
+      src="https://www.freepnglogos.com/uploads/google-logo-png/google-logo-dwglogo-19.png"
+      alt="" width="80"
+      height="80" 
+      class="d-inline-block align-text-center"/>GoogleKeep
+     </a>
+     </div>
+    </nav>
+    </div>
+  );
+}
+
+export default Header;
